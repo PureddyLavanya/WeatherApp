76eaef2c43325be1ab81a93d4fa532310a3c8ddd
PureddyLavanya89 <pureddylavanya2002@gmail.com>
2023-03-15 11:22:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express=require("express")
-var bodyParser=require("body-parser")
-var https=require("https")
+var express=require("express");
+var bodyParser=require("body-parser");
+var https=require("https");
 var app=express();
 
 app.use(bodyParser.urlencoded({extended: true}));
